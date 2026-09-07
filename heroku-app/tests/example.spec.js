c75ac9d1dd927f414c97e0b3e0206134cfe05c5f
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-09-07 12:11:04 +0530

    Refactor Playwright configuration and add login test example

--- heroku-app/tests/example.spec.js
+++ heroku-app/tests/example.spec.js
@@ -1,19 +1,12 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("login test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/login");
+  await page.fill("#username", "tomsmith");
+  await page.fill("#password", "SuperSecretPassword!");
+  await page.click('button[type="submit"]');
+  await expect(page.locator(".flash")).toContainText(
+    "You logged into a secure area",
+  );
 });
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+await page.locator('body').click();
