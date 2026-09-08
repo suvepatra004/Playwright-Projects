dd7188c89b840793f2f3e3ddeb5fef7bd93b42f0
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-09-08 18:32:56 +0530

    Add sidebar locator import and enhance About page validation test

--- saucelabs/tests/productPage.spec.js
+++ saucelabs/tests/productPage.spec.js
@@ -3,6 +3,7 @@ import { BASE_URL, USERNAME, PASSWORD } from "../utils/envConfig";
 import { ProductPage } from "../pages/ProductPage";
 import { LoginPage } from "../pages/LoginPage";
 import { LoginLocators } from "../locators/LoginLocators";
+import { productPageSidebarLocator } from "../locators/ProductPageLocator";
 
 test.describe("Product Validation Page", () => {
   let loginPage;
@@ -16,11 +17,25 @@ test.describe("Product Validation Page", () => {
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
   });
 
+  // Logout
   test("Logout from Product Page", async ({ page }) => {
     productPage = new ProductPage(page);
 
     await productPage.logout();
     // await expect(page.getByRole("button", { name: /login-button/ }));
     await expect(page.locator(LoginLocators.loginButton));
+  });
+
+  // About page
+  test("About page validation", async ({ page }) => {
+    productPage = new ProductPage(page);
+
+    await productPage.openAboutPage();
+    // await expect(
+    //   page.locator(productPageSidebarLocator.aboutPageLoginBtn),
+    // ).toBeVisible();
+    await expect(page.getByRole("button", { text: /Login/ }));
+    await page.goBack();
+    await expect(page.locator(productPageSidebarLocator.menuSidebarOpen));
   });
 });
