b2703551ef29315aeef7276a836a33b73dc4ec28
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-09-13 17:45:02 +0530

    Add CheckoutOverview locator, page class, and test file for checkout overview functionality

---
+++ saucelabs/locators/CheckoutOverviewLocator.js
@@ -0,0 +1,12 @@
+export const CheckoutOverviewLocator = {
+  title: ".title",
+  productNames: ".inventory_item_name",
+  productDesc: ".inventory_item_desc",
+  productPrices: ".inventory_item_price",
+  cancelBtn: "#cancel",
+  finishBtn: "#finish",
+  paymentInfoValue: 'data-test="payment-info-value"',
+  itemPrice: 'data-test="subtotal-label"',
+  tax: 'data-test="tax-label"',
+  total: 'data-test="total-label"',
+};
---
+++ saucelabs/pages/CheckoutOverviewPage.js
@@ -0,0 +1,9 @@
+import { CheckoutOverviewLocator } from "../locators/CheckoutOverviewLocator";
+
+export class CheckoutPageOverview {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async checkoutOverviewPageValidaton() {}
+}
